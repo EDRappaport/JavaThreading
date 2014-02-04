aec80e936a0bad9ec775f3580aba3290ae144220
EDRappaport <ElliRappaport@gmail.com>
2014-02-04 00:05:16 -0500

    multiple threads

--- HelloRunnable.java
+++ HelloRunnable.java
@@ -1,11 +1,18 @@
 public class HelloRunnable implements Runnable {
 
+	int ii;
+	HelloRunnable(int ii){
+		this.ii = ii;
+	}
     public void run() {
-        System.out.println("Hello from a thread!");
+        System.out.println("Hello from a thread!"+ii);
     }
 
     public static void main(String args[]) {
-        (new Thread(new HelloRunnable())).start();
+        (new Thread(new HelloRunnable(0))).start();
+        (new Thread(new HelloRunnable(1))).start();
+        (new Thread(new HelloRunnable(2))).start();
+        (new Thread(new HelloRunnable(3))).start();
     }
 
 }
